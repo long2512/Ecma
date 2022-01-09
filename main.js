90477cea7a746007fbf1b904af40fa804e436287
long2512 <97273073+long2512@users.noreply.github.com>
2022-01-09 20:41:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-import './style.css'
+import '../style.css';
+import { menuList } from './data';
+import { postLisst } from './data';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+const menuItem = document.querySelector('#menu');
+const postItem = document.querySelector('#post');
+const actite = document.querySelector('#actite');
+
+
+
+const showMenu = () =>{
+  const menu = menuList.map((x)=>
+    `
+    <li><a href="" class="menu_link hover:bg-cyan-500 mx-4 text-white">${x}</a></li>
+    `
+  ).join("");
+  if(menuItem){
+    menuItem.innerHTML=menu;
+  }
+  
+}
+showMenu();
+
+const showPost = () =>{
+  const post = postLisst.map((x)=>
+  `
+  <div class="col  border-black-600 p-4 text-left border">
+      <img src="${x.img}" alt="" class="m-auto">
+      <h4 class="uppercase text-[#c97802] py-2 font-medium">${x.title}</h4>
+      <p>${x.text}</p>
+  </div>
+  `
+  ).join("");
+  if(showPost){
+    postItem.innerHTML = post;
+  }
+  if(actite){
+    actite.innerHTML=post;
+  }
+}
+showPost();
