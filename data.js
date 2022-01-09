1416f8eb16efbbde371f15c1e28c18b77ee35bcf
long2512 <97273073+long2512@users.noreply.github.com>
2022-01-09 20:42:33 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+export const menuList = ["Trang chủ","Tuyển sinh","Chương trình đào tạo","Giới Thiệu","Tuyển dụng"];
+export const postLisst = [
+  {
+    img: "picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  },
+  {
+    img: "picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  },
+  {
+    img: "picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  }
+];
