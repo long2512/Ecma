eca8ebe627c10e0fc57b9d17a2a5e44e169f744e
long2512 <buidanglong02@gmail.com>
2022-01-11 14:07:24 +0700

    update lab2

--- pages/detailNew.js
+++ pages/detailNew.js
@@ -1,8 +1,9 @@
-import { data } from "autoprefixer"
+import { postLisst } from "../src/data";
+
 
 const DetailNewPage = {
     print(id){
-        const result = data.find((post) => post.id === id);
+        const result = postLisst.find((post) => post.id === id);
         return`
             <img src="${result.img}" alt="" class="m-auto">
             <h4 class="uppercase text-[#c97802] py-2 font-medium">${result.title}</h4>
