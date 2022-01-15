613e2bee9fd0e83041dc799281e32eed30e14db3
long2512 <buidanglong02@gmail.com>
2022-01-15 12:26:33 +0700

    add vercel.json

--- src/data.js
+++ src/data.js
@@ -39,5 +39,23 @@ export const postLisst = [
     img: "http://picsum.photos/300/200",
     title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
     text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  },
+  {
+    id:"4",
+    img: "http://picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  },
+  {
+    id:"5",
+    img: "http://picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
+  },
+  {
+    id:"6",
+    img: "http://picsum.photos/300/200",
+    title: "Vinh danh 295 sinh viên Fpoly học kỳ Spring 2018",
+    text: "Với sự tham gia của các của rất trong các chuyên gia trong nhiều lĩnh vực ,kinh doanh,chương trình đã thu hút đông đảo sinh viên Fpoy tham dự"
   }
 ];
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -72,10 +72,21 @@ const Admin = {
                                 <div class="text-sm text-gray-900">${post.title}</div>
                               </td>
                               <td class="px-6 py-4 whitespace-nowrap">
-                                <a href="/editNews/${post.id}"><div class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-lime-700 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Edit</div></a>
+                                <a href="/editNews/${post.id}"><div class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-lime-700 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                                </svg>
+                                </div></a>
                               </td>
                               <td class="pm">
-                              <a href=""><div class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Delete</div></a>
+                              <a href=""><div class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                              </svg></div></a>
+
+
+                              
+
+
                               </td>
                             </tr>
                               `
