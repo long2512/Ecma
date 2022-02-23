754c64a8799653518a4b792de33d57c5c68a34ab
long2512 <buidanglong02@gmail.com>
2022-02-23 12:38:42 +0700

    update cart

--- src/utils/reRender.js
+++ src/utils/reRender.js
@@ -1,6 +1,6 @@
 export const reRender = async (component, dom) => {
     if(component){
-        document.querySelector(dom).innerHTML = await component.render();
+        document.querySelector(dom).innerHTML = await component.print();
     }
     if(component.afterRender) await component.afterRender();
 }
